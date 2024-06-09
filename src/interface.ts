f2aedf3c5ed6ab617d2d29fbceaa422c74032783
iamKhang <iamhoangkhang@icloud.com>
2024-06-09 14:52:04 +0700

    Tái cấu trúc dự án

--- src/interface.ts
+++ src/interface.ts
@@ -1,20 +1,4 @@
 export interface Pokemon {
-    id: number;
-    name: string;
-    sprites: {
-      front_default: string;
-    };
-  }
-
-export  interface Detail{
-    id: number;
-    isOpened: boolean;
-  }
-
-export interface PokemonDetail extends Pokemon {
-    abilities?: {
-      ability: string;
-      name: string;
-    }[];
+  name: string;
+  url: string;
 }
-  
